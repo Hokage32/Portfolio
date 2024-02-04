88b9608c3ad10171e7819142baa85e59bcb9c87d
Levi Raoelina <levi_r34@yahoo.com>
2024-02-04 15:57:58 -0700

    not working

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,16 +8,13 @@ const Header = () => {
 
   const handleClick = () => {
     setHamburger(!hamburger);
-    
   };
   return hamburger ? (
     <div className="hamburger-menu-container flex flex-col items-center w-screen h-20 relative z-50">
       <div className="hamburger-menu flex flex-col justify-center items-center w-96 h-96  bg-gray-500 bg-opacity-75 relative z-50  p-6 rounded-bl-3xl rounded-br-3xl ">
-        
-       
-          <button>Contact</button>
-          <button>Projects</button>
-        
+        <button>Contact</button>
+        <button>Projects</button>
+
         <button onClick={handleClick}>Close</button>
       </div>
     </div>
